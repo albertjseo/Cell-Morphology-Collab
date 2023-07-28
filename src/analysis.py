1e88cb90a2374f73e9e357753ce98baeae59418f
Kevin Chang <kevinjchang98@gmail.com>
2023-07-28 02:14:01 -0700

    Move app source code to src folder

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,4 @@
+import pandas
 from pandastable import TableModel
 
 
@@ -15,8 +16,22 @@ class Analysis:
         self.image_path = image_path
         self.cell_measurements = None
         self.results_are_updated = False
+        self.image = None
 
-    def set_image_path(self, image_path):
+    def load_image(self, image_path) -> bool:
+        """
+        TODO: Finish
+            It should:
+                - Update the class's image_path with the new one after
+                validating
+                - Load the image using OpenCV and save it as the class's
+                image var
+                - Delete the old results and update class state
+                - Return a True if successful or False if not successful
+        :return:
+        """
+
+    def set_image_path(self, image_path) -> None:
         """
         Sets a new image path. Paths are expected to be valid. Will set
         results_are_updated to false if a new image path is supplied
@@ -30,7 +45,7 @@ class Analysis:
 
         self.results_are_updated = False
 
-    def run_analysis(self):
+    def run_analysis(self) -> None:
         """
         TODO: Put the main analysis logic here
         :return:
@@ -43,7 +58,7 @@ class Analysis:
 
         self.results_are_updated = True
 
-    def get_cell_measurements(self):
+    def get_cell_measurements(self) -> pandas.DataFrame or None:
         """
         Returns a Pandas DataFrame of cell measurement information
 
@@ -51,3 +66,5 @@ class Analysis:
         """
         if self.results_are_updated:
             return self.cell_measurements
+        else:
+            return None
